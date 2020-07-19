7650b1f9ae494c4950b256d5e4098bd280d2bb0a
TesseraSkye <39166259+TesseraSkye@users.noreply.github.com>
2020-07-19 08:41:36 -0700

    Started addingdependencies

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,15 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import osc from 'osc'
+// import midi from 'midi'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.use(osc)
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
